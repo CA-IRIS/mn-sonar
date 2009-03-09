1929f44acdafc88ab3ad9ebaf80e2125fd4c0f71
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2009-03-09 16:33:49 -0500

    Added OpenLDAP configuration stuff.

---
+++ etc/setup_openldap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+yum install openldap-servers
+cp ./slapd-test.conf /etc/openldap/slapd.conf
+/sbin/service ldap start
+sleep 5
+ldapadd -x -D 'cn=admin,dc=sonar' -w password -f sonar-test.ldif
