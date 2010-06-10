433afe903d59e0d6143f745583f18c858008e600
Douglas Lau (doug.lau@state.mn.us) <Douglas Lau (doug.lau@state.mn.us)>
2010-06-10 09:37:26 -0500

    Provide exception detail if keystore cannot be read.

--- src/us/mn/state/dot/sonar/Security.java
+++ src/us/mn/state/dot/sonar/Security.java
@@ -1,6 +1,6 @@
 /*
  * SONAR -- Simple Object Notification And Replication
- * Copyright (C) 2006-2009  Minnesota Department of Transportation
+ * Copyright (C) 2006-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ public class Security {
 			}
 			catch(IOException ee) {
 				throw new ConfigurationError("Cannot read " +
-					keystore);
+					keystore + ", " + e.getMessage());
 			}
 		}
 	}
